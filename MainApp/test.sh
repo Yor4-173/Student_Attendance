b5895bdeb1cf6d6c42bd98c59d9d7b91b4623d76
Phạm Thiều Gia Khang <giakhangvtaqn@gmail.com>
2025-04-26 19:24:35 +0700

    remove/term

--- MainApp/test.sh
+++ MainApp/test.sh
@@ -3,4 +3,4 @@ kafka-topics --create --bootstrap-server 192.168.43.58:9093 replication-factor 1
 kafka-console-producer   --broker-list 192.168.43.58:9093   --topic TEST   --producer.config /etc/kafka/secrets/run.properties
 kafka-console-consumer   --bootstrap-server 192.168.43.58:9093   --topic TEST --from-beginning --partition 0   --consumer.config /etc/kafka/secrets/run.properties
 
-kafka-topics --create --bootstrap-server 192.168.43.58:9093 replication-factor 1 --partitions 1 --topic receive_result  --command-config /etc/kafka/secrets/run.properties
+kafka-topics --create --bootstrap-server 192.168.43.58:9093 replication-factor 1 --partitions 1 --topic SEND  --command-config /etc/kafka/secrets/run.properties
